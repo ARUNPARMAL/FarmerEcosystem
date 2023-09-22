5e4ba95ee2cd9a673738501e5e129e8750393c64
ekta agarwal <ektaag369@gmail.com>
2023-09-22 13:19:43 +0530

    NewUpdateCommit

--- app/src/main/java/com/arunparmal/farmerecosystem/activities/auth/MainActivity2.java
+++ app/src/main/java/com/arunparmal/farmerecosystem/activities/auth/MainActivity2.java
@@ -6,16 +6,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.arunparmal.farmerecosystem.R;
 import com.arunparmal.farmerecosystem.adapters.Seedsadapter;
 import com.arunparmal.farmerecosystem.utility.Constants;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
@@ -28,6 +36,9 @@ public class MainActivity2 extends AppCompatActivity {
     Button get_otp;
     EditText mobielnumber;
     ProgressBar progressBar;
+    ImageView loginGoogle;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +47,18 @@ public class MainActivity2 extends AppCompatActivity {
         get_otp=findViewById(R.id.btn_get_otp);
         mobielnumber=findViewById(R.id.input_mobileno);
         progressBar=findViewById(R.id.progressbarsending);
+
+        loginGoogle=findViewById(R.id.googlelogobtn);
+        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc= GoogleSignIn.getClient(this,gso);
+
+        loginGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googlesignin();
+
+            }
+        });
 
         get_otp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -95,6 +118,31 @@ public class MainActivity2 extends AppCompatActivity {
 
 
 
+    }
+
+    private void googlesignin() {
+//        Intent i=new Intent(MainActivity2.this,M.class);
+//        startActivity(i);
+//        Intent signInIntent=gsc.getSignInIntent();
+//        startActivityForResult(signInIntent,1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1000){
+            Task<GoogleSignInAccount> task=GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                startActivity(new Intent(MainActivity2.this,FarmerDetailsActivity.class));
+                finish();
+            } catch (ApiException e) {
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 
 }
